397328a495087b8bb0ac75d3ef5192348d242fe6
umarsani1605 <63645229+umarsani1605@users.noreply.github.com>
2025-02-03 10:29:33 +0000

    Deploying to gh-pages from @ umarsani1605/nuxt-content-starter@be4a731affc6286f233bcb9cf127e2ca88e8596b 🚀

---
+++ _nuxt/DpjChaYO.js
@@ -0,0 +1,1 @@
+import{a as r,f as a,k as o,U as s,af as l,l as n}from"./oVdGDvos.js";const c=r({__name:"Alert",props:{color:{type:String,default:"orange"}},setup(e){return(t,d)=>(o(),a("div",{class:"alert",style:l({"border-color":e.color})},[s(t.$slots,"default",{},void 0,!0)],4))}}),f=Object.assign(n(c,[["__scopeId","data-v-46dbbe98"]]),{__name:"Alert"});export{f as default};
